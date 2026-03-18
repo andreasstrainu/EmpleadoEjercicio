afffbd3bdf4896453514a6ad7927a9361697b2d9
SOFTTEKPROJECTS\fabiola.novoa <fabiola.novoa@softtek.com>
2026-03-18 16:57:42 +0100

    Creacion de menu, solo las opciones, sin implementar

--- src/main/java/org/example/ui/Menu.java
+++ src/main/java/org/example/ui/Menu.java
@@ -1,4 +1,41 @@
 package org.example.ui;
 
+import org.example.service.Directorio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Scanner;
+
 public class Menu {
+
+    public void menu() {
+        Scanner scan = new Scanner(System.in);
+
+        Properties props = new Properties();
+
+        try (InputStream input = Menu.class.getClassLoader()
+                .getResourceAsStream("version.properties")) {
+            if (input == null) {
+                System.out.println("No se encontró el archivo version.properties");
+                return;
+            }
+            props.load(input);
+            String version = props.getProperty("version", "desconocida");
+            System.out.println("*********Directorio Corporativo " + "(V<"+ version + ">)*********");
+
+            System.out.println(" 1. Imprimir directorio ");
+            System.out.println(" 2. Buscar usuario ");
+            System.out.println(" 3. Guardar directorio");
+            System.out.println(" 4. Leer directorio ");
+            System.out.println(" Escoge una opción: ");
+            
+
+
+        } catch (IOException e) {
+            System.err.println("Error leyendo la versión: " + e.getMessage());
+        }
+
+    }
+
 }
