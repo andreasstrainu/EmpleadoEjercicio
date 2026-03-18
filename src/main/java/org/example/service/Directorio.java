577fe25379ef6d10237b44759dfeadacc626ed60
SOFTTEKPROJECTS\sorin.strainu <andreas.strainu@softtek.com>
2026-03-18 17:20:18 +0100

    Terminados los metodos de json en Directorio

--- src/main/java/org/example/service/Directorio.java
+++ src/main/java/org/example/service/Directorio.java
@@ -3,6 +3,7 @@ package org.example.service;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.example.model.Empleado;
+import org.example.util.JsonUtils;
 
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -35,8 +36,26 @@ public class Directorio {
         }
     }
     public void guardarJSON(String ruta){
-
+        try (FileWriter writer = new FileWriter(ruta)) {
+            String json = JsonUtils.directorioToJSON(this);
+            writer.write(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void cargarJSON(String ruta){
+        try (FileReader reader = new FileReader(ruta)) {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                sb.append((char) c);
+            }
+
+            Directorio d = JsonUtils.jsonToDirectorio(sb.toString());
+            this.empleados = d.empleados;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
