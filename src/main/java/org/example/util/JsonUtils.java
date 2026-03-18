aedbd2092f356cfed5b055be2cf7ba2d80381268
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-03-18 17:12:34 +0100

    jsonutils listo

--- src/main/java/org/example/util/JsonUtils.java
+++ src/main/java/org/example/util/JsonUtils.java
@@ -1,6 +1,24 @@
 package org.example.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.example.service.Directorio;
+
 public class JsonUtils {
 
+    // Gson configurado para toda la clase
+    private static final Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .setDateFormat("yyyy-MM-dd")
+            .create();
 
+    // Convertir Directorio → JSON
+    public static String directorioToJSON(Directorio d) {
+        return gson.toJson(d);
+    }
+
+    // Convertir JSON → Directorio
+    public static Directorio jsonToDirectorio(String json) {
+        return gson.fromJson(json, Directorio.class);
+    }
 }
