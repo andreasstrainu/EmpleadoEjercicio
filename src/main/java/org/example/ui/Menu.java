decc32f5d84c2901e4d74eac2594f9e034d76d0d
SOFTTEKPROJECTS\fabiola.novoa <fabiola.novoa@softtek.com>
2026-03-18 17:27:53 +0100

    menu medio implementado, falta guardar y leer directorio

--- src/main/java/org/example/ui/Menu.java
+++ src/main/java/org/example/ui/Menu.java
@@ -1,9 +1,12 @@
 package org.example.ui;
 
+import org.example.model.Empleado;
 import org.example.service.Directorio;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -11,6 +14,8 @@ public class Menu {
 
     public void menu() {
         Scanner scan = new Scanner(System.in);
+        List<Empleado> empleados = new ArrayList<>();
+        Directorio directorio = new Directorio(empleados);
 
         Properties props = new Properties();
 
@@ -28,9 +33,29 @@ public class Menu {
             System.out.println(" 2. Buscar usuario ");
             System.out.println(" 3. Guardar directorio");
             System.out.println(" 4. Leer directorio ");
+            System.out.println(" 5. Salir");
             System.out.println(" Escoge una opción: ");
-            
+            String opcion = scan.nextLine();
 
+            switch (opcion) {
+                case "1":
+                    directorio.imprimir();
+                    break;
+                case "2":
+                    System.out.println("Nombre de Empleado: ");
+                    String nombreEmpleado = scan.nextLine();
+                    directorio.buscarPorNombre(nombreEmpleado);
+                    break;
+                case "3":
+                    directorio.guardarJSON();
+                    break;
+                case "4":
+
+                    break;
+                default:
+                    System.out.println("Saliendo...");
+                    break;
+            }
 
         } catch (IOException e) {
             System.err.println("Error leyendo la versión: " + e.getMessage());
